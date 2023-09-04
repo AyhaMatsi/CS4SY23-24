a39e01f8e5693e14e713c6a7e86e38184050da30
PHOTON <PHOTON@172.22.16.64>
2023-09-04 12:59:18 +0800

    complete Ex1

---
+++ src/Q1_Excersize_2/Song.java
@@ -0,0 +1,15 @@
+package Q1_Excersize_2;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author PHOTON
+ */
+public class Song {
+    
+}
